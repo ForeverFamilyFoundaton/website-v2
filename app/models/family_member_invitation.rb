9a12791d0286262ff625b8d32286394a9a7e4745
Dinshaw <gobhai@gmai.com>
2020-03-20 15:40:09 -0600

    invites

--- app/models/family_member_invitation.rb
+++ app/models/family_member_invitation.rb
@@ -9,4 +9,13 @@ class FamilyMemberInvitation
   validates :role, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validate :email_is_unique
+
+  private
+
+  def email_is_unique
+    if User.where(email: email).exists?
+      errors.add(:email, 'has been taken.')
+    end
+  end
 end
