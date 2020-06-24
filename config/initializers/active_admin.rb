0ec0d4d0244069777681a90b45c60a2f6aa7a893
Dinshaw <gobhai@gmai.com>
2020-06-24 15:26:18 -0600

    internal layout

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.setup do |config|
+  meta_tags_options = { viewport: 'width=device-width, initial-scale=1' }
+  config.meta_tags = meta_tags_options
+  config.meta_tags_for_logged_out_pages = meta_tags_options
   # == Site Title
   #
   # Set the title that is displayed on the main layout
@@ -9,7 +12,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
