87f4e18f8e920c5ac7688bfd8a365674b0c091c0
Dinshaw <gobhai@gmai.com>
2021-01-05 21:35:29 -0700

    Fix events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,15 @@
 module EventsHelper
+  def event_times(event)
+    if event.start_time.day == event.end_time.day
+      [
+        I18n.l(event.start_time, format: :event_date),
+        [
+          I18n.l(event.start_time, format: :event_time),
+          I18n.l(event.end_time, format: :event_time)
+        ].join(' to ')
+      ].join(' ')
+    else
+      [start_time.to_s(:events), end_time.to_s(:events)].join(' to ')
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,6 @@ class Event < ApplicationRecord
   scope :upcoming, -> { where("end_time >= ?", Time.zone.now).order('start_time asc').limit(6) }
   scope :all_upcoming, -> { where("end_time >= ?", Time.zone.now).order('start_time asc') }
 
-  def times
-    if start_time.day == end_time.day
-      "#{start_time.to_s(:ordinal_date)} " + [start_time.strftime("%l:%M %p"), end_time.strftime("%l:%M %p")].join(' - ')
-    else
-      [start_time.to_s(:events), end_time.to_s(:events)].join(' to ')
-    end
-  end
-
   def teaser
     truncate strip_tags(description)
   end
