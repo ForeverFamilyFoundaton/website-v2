d1eaffe5402989f796c0a4b6dd8069c83f689b63
Dinshaw Gobhai <gobhai@gmail.com>
2021-04-17 21:09:46 -0600

    Added CmsPages.nav_item flag

--- app/admin/cms_pages.rb
+++ app/admin/cms_pages.rb
@@ -3,8 +3,9 @@ ActiveAdmin.register CmsPage do
   filter :title
   filter :reference_string
   filter :parent, input_html: { class: 'js-select' }
+  filter :nav_item
 
-  permit_params :reference_string, :title, :sub_title, :body, :parent_id
+  permit_params :reference_string, :title, :sub_title, :body, :parent_id, :nav_item
 
   controller do
     def find_resource
@@ -20,6 +21,7 @@ ActiveAdmin.register CmsPage do
     column :parent do |cms_page|
       cms_page.parent&.title
     end
+    column :nav_item
     column :updated_at
     actions
   end
@@ -29,6 +31,7 @@ ActiveAdmin.register CmsPage do
       f.input :reference_string, input_html: { disabled: !object.new_record? }
       f.input :parent, collection: CmsPage.order(reference_string: :asc).pluck(:reference_string, :id), input_html: { class: 'js-select' }, include_blank: true
       f.input :title
+      f.input :nav_item
       f.input :sub_title
       f.input :body, label: false, input_html: { class: [:code, :markdown] }
       div do
@@ -44,6 +47,7 @@ ActiveAdmin.register CmsPage do
     div cms_page.parent.reference_string if cms_page.parent.present?
     h1 cms_page.title
     h2 cms_page.sub_title
+    h2 cms_page.nav_item
     div cms_page.reference_string
     div do
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
--- app/models/cms_page.rb
+++ app/models/cms_page.rb
@@ -9,6 +9,8 @@ class CmsPage < ApplicationRecord
   validates :reference_string, presence: true, uniqueness: true
   validates :title, presence: true
 
+  scope :nav_items, -> { where(nav_item: true) }
+
   def partial_name
     slug.underscore
   end
