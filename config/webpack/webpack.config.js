9dfa3d42c5d9f9742527116c451fd3c021396767
Dinshaw Gobhai <gobhai@gmail.com>
2022-11-29 20:27:14 -0700

    Updates to get Shakapacker working

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,18 @@
 const { webpackConfig } = require("shakapacker");
+const { merge } = require("webpack-merge");
 
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-
-module.exports = webpackConfig;
+const customConfig = {
+  module: {
+    rules: [
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+    ],
+  },
+};
+module.exports = merge(webpackConfig, customConfig);
