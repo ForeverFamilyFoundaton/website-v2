1c3fe8659df5924041c4d973006365c3981b38d8
willadmin78 <wriccio00@gmail.com>
2023-08-20 16:43:03 +0200

    301 signs of life

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ ForeverFamilyFoundation::Application.routes.draw do
   root to: 'site#index'
   # Need this for legacy links hard-coded into Love Knows No Death PDF
   get 'site/page/:id', to: 'site#page'
+  get '/pages/signs-of-life-magazine', to redirect('/', status: 301)
+  
 
   devise_for :users, controllers: {
     registrations: 'registrations',
